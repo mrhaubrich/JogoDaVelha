f83ee18fc4a48394c8fb222a2c5c0a086cad577c
Marco Haubrich <marcolino.haubrich@gmail.com>
2021-10-01 08:43:58 -0300

    Abrindo a camera ao clicar, TODO:colocar icone da foto no botao

--- app/src/main/java/ucs/exercicio1/jogodavelha/MainActivity.java
+++ app/src/main/java/ucs/exercicio1/jogodavelha/MainActivity.java
@@ -1,22 +1,33 @@
 package ucs.exercicio1.jogodavelha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE_JOGADOR1 = 1;
+    static final int REQUEST_IMAGE_CAPTURE_JOGADOR2 = 2;
     ImageButton[] positionArray = new ImageButton[9];
     Button btnClear;
+    ImageButton btnJogador1, btnJogador2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnClear = findViewById(R.id.btnClear);
-        instanciaPosition();
+        instanciaElementos();
+
     }
     private boolean ganhador(View view){
         // if posicao for impar ou 4 verificar os lados
@@ -25,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         // posicao +- 4 e posicao +- 8
         return false;
     }
-    private void instanciaPosition(){
+    private void instanciaElementos(){
         for (int i = 0; i<9; i++){
             // pega o id do xml
             String positionID = "imgPos" + i;
@@ -40,6 +51,48 @@ public class MainActivity extends AppCompatActivity {
                 position.setBackgroundResource(R.mipmap.dog_foreground);
                 btnClear.setText(text);
             });
+        }
+        btnJogador1 = findViewById(R.id.imgPlayer1);
+        btnJogador2 = findViewById(R.id.imgPlayer2);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE);
+        btnJogador1.setOnClickListener(view -> {
+            // if imagem não existe abre camera else inicia jogo
+            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE_JOGADOR1);
+                return;
+            }
+            requestPermissions(new String[]{Manifest.permission.CAMERA},666);
+        });
+        btnJogador2.setOnClickListener(view -> {
+            // if imagem não existe abre camera else inicia jogo
+            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE_JOGADOR2);
+                return;
+            }
+            requestPermissions(new String[]{Manifest.permission.CAMERA},666);
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case REQUEST_IMAGE_CAPTURE_JOGADOR1:
+                Log.i("TAG", "onActivityResult: JOGADOR1");
+                if (resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    btnJogador1.setImageBitmap(imageBitmap);
+                }
+                break;
+            case REQUEST_IMAGE_CAPTURE_JOGADOR2:
+                Log.i("TAG", "onActivityResult: JOGADOR2");
+                if (resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    btnJogador2.setImageBitmap(imageBitmap);
+                }
+                break;
         }
     }
 }
