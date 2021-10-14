1a72b00f8942c97719971f42c6c934c3f116d34f
Marco TCS <marco@tcssistemas.com.br>
2021-10-14 09:57:14 -0300

    Arquivos deletam +  seleção de quem inicia o jogo quando seleciona a foto

--- app/src/main/java/ucs/exercicio1/jogodavelha/MainActivity.java
+++ app/src/main/java/ucs/exercicio1/jogodavelha/MainActivity.java
@@ -1,16 +1,10 @@
 package ucs.exercicio1.jogodavelha;
 
 import android.Manifest;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.PointF;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -26,22 +20,22 @@ import android.media.FaceDetector;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE_PLAYER_1 = 1;
-    static final int REQUEST_IMAGE_CAPTURE_PLAYER_2 = 2;
+    static final int PLAYER_1 = 1;
+    static final int PLAYER_2 = 2;
+    Bitmap bmpPlayer1, bmpPlayer2;
+    private boolean gameStarted = false;
     static final int numberOfFaces = 1;
     private FaceDetector myFaceDetect = null;
     private FaceDetector.Face[] myFace = null;
     float myEyesDistance;
     int numberOfFaceDetected = 0;
     String currentPhotoPathPlayer1, currentPhotoPathPlayer2;
-    int currentPlayer = 1;
+    int currentPlayer = 0;
     ImageButton[][] positionMatrix = new ImageButton[3][3];
     int[][] tabuleiro = new int[3][3];
     Button btnClear;
@@ -62,18 +56,18 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode != RESULT_OK) return;
         switch (requestCode){
-            case REQUEST_IMAGE_CAPTURE_PLAYER_1:
-                galleryAddPic(REQUEST_IMAGE_CAPTURE_PLAYER_1);
+            case PLAYER_1:
+                galleryAddPic(PLAYER_1);
                 try {
-                    setPic(btnJogador1, REQUEST_IMAGE_CAPTURE_PLAYER_1);
+                    setPic(btnJogador1, PLAYER_1);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 break;
-            case REQUEST_IMAGE_CAPTURE_PLAYER_2:
-                galleryAddPic(REQUEST_IMAGE_CAPTURE_PLAYER_2);
+            case PLAYER_2:
+                galleryAddPic(PLAYER_2);
                 try {
-                    setPic(btnJogador2, REQUEST_IMAGE_CAPTURE_PLAYER_2);
+                    setPic(btnJogador2, PLAYER_2);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -93,6 +87,68 @@ public class MainActivity extends AppCompatActivity {
 
     private void instanciaElementos() {
         btnClear.setOnClickListener(view -> clear());
+
+        instanciaBtnTabuleiro();
+
+        btnJogador1 = findViewById(R.id.imgPlayer1);
+        btnJogador2 = findViewById(R.id.imgPlayer2);
+
+        btnJogador1.setOnClickListener(view -> {
+            // if imagem não existe abre camera else inicia jogo
+            if(bmpPlayer1 == null){
+                abreCameraIntent(PLAYER_1);
+            }
+            else{
+                //iniciar jogo
+                if(bmpPlayer2 == null) {
+                    Toast.makeText(this,"Ambos jogadores precisam tirar foto!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(!gameStarted){
+                    currentPlayer = PLAYER_1;
+                    setTabuleiroEnabled(true);
+                }
+            }
+        });
+        btnJogador2.setOnClickListener(view -> {
+            // if imagem não existe abre camera else inicia jogo
+            if(bmpPlayer2 == null){
+                abreCameraIntent(PLAYER_2);
+            }
+            else{
+                //iniciar jogo
+                if(bmpPlayer1 == null) {
+                    Toast.makeText(this,"Ambos jogadores precisam tirar foto!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(!gameStarted){
+                    currentPlayer = PLAYER_2;
+                    setTabuleiroEnabled(true);
+                }
+            }
+        });
+    }
+
+    private void abreCameraIntent(int player){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE);
+        cameraIntent.removeExtra(MediaStore.EXTRA_OUTPUT);
+        File photoFile = null;
+        try {
+            photoFile = createImageFile(player);
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            Toast.makeText(this.getApplicationContext(),"Error creating file",Toast.LENGTH_LONG).show();
+        }
+        if (photoFile == null) return;
+        Uri photoURI = FileProvider.getUriForFile(this,
+                "com.example.android.fileprovider",
+                photoFile);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        if(cameraIntent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(cameraIntent, player);
+    }
+
+    private void instanciaBtnTabuleiro(){
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 // region Somador para pegar posicao
@@ -141,63 +197,21 @@ public class MainActivity extends AppCompatActivity {
                     imageButtonPositionOnMatrix = null;
                     alternarPlayer();
                 });
+                positionMatrix[i][j].setEnabled(false);
             }
         }
-        btnJogador1 = findViewById(R.id.imgPlayer1);
-        btnJogador2 = findViewById(R.id.imgPlayer2);
-
-
-        btnJogador1.setOnClickListener(view -> {
-            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE);
-            // if imagem não existe abre camera else inicia jogo
-            cameraIntent.removeExtra(MediaStore.EXTRA_OUTPUT);
-            File photoFile = null;
-            try {
-                photoFile = createImageFile(REQUEST_IMAGE_CAPTURE_PLAYER_1);
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                Toast.makeText(this.getApplicationContext(),"Error creating file",Toast.LENGTH_LONG).show();
-            }
-            if (photoFile == null) return;
-            Uri photoURI = FileProvider.getUriForFile(this,
-                    "com.example.android.fileprovider",
-                    photoFile);
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-            try {
-                if(cameraIntent.resolveActivity(getPackageManager()) != null)
-                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE_PLAYER_1);
-            } catch (ActivityNotFoundException e) {
-                // display error state to the user
-            }
-        });
-        btnJogador2.setOnClickListener(view -> {
-            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE);
-            // if imagem não existe abre camera else inicia jogo
-            cameraIntent.removeExtra(MediaStore.EXTRA_OUTPUT);
-            File photoFile = null;
-            try {
-                photoFile = createImageFile(REQUEST_IMAGE_CAPTURE_PLAYER_2);
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                Toast.makeText(this.getApplicationContext(),"Error creating file",Toast.LENGTH_LONG).show();
-            }
-            if (photoFile == null) return;
-            Uri photoURI = FileProvider.getUriForFile(this,
-                    "com.example.android.fileprovider",
-                    photoFile);
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-            try {
-                if(cameraIntent.resolveActivity(getPackageManager()) != null)
-                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE_PLAYER_2);
-            } catch (ActivityNotFoundException e) {
-                // display error state to the user
-            }
-        });
     }
-
     //endregion
 
     //region Gameplay
+    private void setTabuleiroEnabled(boolean enabled){
+        for(int i = 0 ; i < 3 ; i++){
+            for(int j = 0 ; j < 3 ; j++){
+                positionMatrix[i][j].setEnabled(enabled);
+            }
+        }
+    }
+
     private int[] getImageButtonPositionOnMatrix(ImageButton imgBtn){
         for(int i = 0 ; i < 3 ; i++){
             for(int j = 0 ; j < 3 ; j++){
@@ -290,6 +304,8 @@ public class MainActivity extends AppCompatActivity {
         }
         btnJogador1.setImageResource(R.drawable.jogador1);
         btnJogador2.setImageResource(R.drawable.jogador2_btn_foreground);
+        bmpPlayer1 = null;
+        bmpPlayer2 = null;
     }
     private boolean isVelha(){
         for(int i = 0; i < 3 ; i++){
@@ -304,17 +320,24 @@ public class MainActivity extends AppCompatActivity {
 
     //region Arquivos
 
+    private boolean deletePicsFromGallery(String photoPath){
+        File fdelete = new File(photoPath);
+        if (fdelete.exists()) {
+            return fdelete.delete();
+        }
+        return false;
+    }
 
     private void galleryAddPic(int player) {
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(player == REQUEST_IMAGE_CAPTURE_PLAYER_1 ? currentPhotoPathPlayer1 : currentPhotoPathPlayer2);
+        File f = new File(player == PLAYER_1 ? currentPhotoPathPlayer1 : currentPhotoPathPlayer2);
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
     }
 
     private void setPic(ImageButton btnJogador, int player) throws IOException {
-        String currentPhotoPath = player == REQUEST_IMAGE_CAPTURE_PLAYER_1 ? currentPhotoPathPlayer1 : currentPhotoPathPlayer2;
+        String currentPhotoPath = player == PLAYER_1 ? currentPhotoPathPlayer1 : currentPhotoPathPlayer2;
         Log.i("path", "path: " + currentPhotoPath);
         // Get the dimensions of the View
         int targetW = btnJogador.getWidth();
@@ -343,7 +366,10 @@ public class MainActivity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
         bitmap = cropBitmap(bitmap);
         bitmap = rotateBitmap(bitmap,orientation);
+        if(player == PLAYER_1) bmpPlayer1 = bitmap;
+        else bmpPlayer2 = bitmap;
         btnJogador.setImageBitmap(bitmap);
+        if(!deletePicsFromGallery(currentPhotoPath)) Toast.makeText(this,"Não foi possível deletar as imagens!",Toast.LENGTH_LONG).show();
     }
 
     private Bitmap cropBitmap(Bitmap srcBmp){
@@ -404,7 +430,7 @@ public class MainActivity extends AppCompatActivity {
         File image = new File(storageDir, imageFileName + ".jpg");
 
         // Save a file: path for use with ACTION_VIEW intents
-        if(player == REQUEST_IMAGE_CAPTURE_PLAYER_1)
+        if(player == PLAYER_1)
             currentPhotoPathPlayer1 = image.getAbsolutePath();
         else
             currentPhotoPathPlayer2 = image.getAbsolutePath();
